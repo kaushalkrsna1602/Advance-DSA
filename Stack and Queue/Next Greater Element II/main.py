c6ae6f46bd5a1316984e9fe636868e6ee8c68b6c
Kaushal kumar <kaushal.kr.kr@gmail.com>
2025-09-11 11:18:54 +0530

    Next Greater Element II [py] -2

--- Stack and Queue/Next Greater Element II/main.py
+++ Stack and Queue/Next Greater Element II/main.py
@@ -16,3 +16,24 @@ class Solution:
             stack.append(arr[i])
         return ans[0:n//2]
         
+
+
+# WIthout circular
+
+class Solution:
+    def nextGreaterElements(self, arr: List[int]) -> List[int]:
+        n = len(arr)
+        stack = []
+        ans = [-1]*n
+        stack.append(arr[n - 1])
+        for i in range((2* n) - 2, -1, -1):
+            while len(stack):
+                top = stack[len(stack) - 1]
+                if arr[i % n] < top:
+                    ans[i % n] = top
+                    break
+                else:
+                    stack.pop()
+            stack.append(arr[i % n])
+        return ans[0:n]
+        

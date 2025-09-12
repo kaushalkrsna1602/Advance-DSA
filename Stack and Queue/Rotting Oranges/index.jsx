d07bc1d74a9779d8d304e5e7938bc6103e11b489
Kaushal kumar <kaushal.kr.kr@gmail.com>
2025-09-12 12:03:26 +0530

    Rotting Oranges [ js ]

---
+++ Stack and Queue/Rotting Oranges/index.jsx
@@ -0,0 +1,59 @@
+// Test Result
+// 994. Rotting Oranges
+
+// You are given an m x n grid where each cell can have one of three values:
+
+// 0 representing an empty cell,
+// 1 representing a fresh orange, or
+// 2 representing a rotten orange.
+// Every minute, any fresh orange that is 4-directionally adjacent to a rotten orange becomes rotten.
+
+// Return the minimum number of minutes that must elapse until no cell has a fresh orange. If this is impossible, return -1.
+
+
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+var orangesRotting = function(grid) {
+    let m = grid.length;
+    let n = grid[0].length;
+    let queue = [];
+    for (let i = 0; i < m; i++) {
+        for (let j = 0; j < n; j++) {
+            if (grid[i][j] === 2) {
+                queue.push([i, j, 0]);
+            }
+        }
+    }
+    let maxMinutes = 0;
+    while (queue.length) {
+        let [x, y, level] = queue.shift();
+
+        if (x > 0 && grid[x - 1][y] === 1) {
+            grid[x - 1][y] = 2;
+            queue.push([x - 1, y, level + 1]);
+        }
+        if (x < m - 1 && grid[x + 1][y] === 1) {
+            grid[x + 1][y] = 2;
+            queue.push([x + 1, y, level + 1]);
+        }
+        if (y < n - 1 && grid[x][y + 1] === 1) {
+            grid[x][y + 1] = 2;
+            queue.push([x, y + 1, level + 1]);
+        }
+        if (y > 0 && grid[x][y - 1] === 1) {
+            grid[x][y - 1] = 2;
+            queue.push([x, y - 1, level + 1]);
+        }
+        maxMinutes = Math.max(level, maxMinutes);
+    }
+    for (let i = 0; i < m; i++) {
+        for (let j = 0; j < n; j++) {
+            if (grid[i][j] === 1) {
+                return -1;
+            }
+        }
+    }
+    return maxMinutes;
+};

6a80fbfa304da6e78368538709aadb5976146e77
Kaushal kumar <kaushal.kr.kr@gmail.com>
2025-09-19 11:02:11 +0530

    Search in Rotated Sorted Array [ py ]

---
+++ Binary Search/Search in Rotated Sorted Array/main.py
@@ -0,0 +1,19 @@
+class Solution:
+    def search(self, arr: List[int], target: int) -> int:
+        l = 0
+        r = len(arr) - 1
+        while l <= r:
+            m = l + ((r - l) // 2)
+            if target == arr[m]:
+                return m
+            if arr[l] <= arr[m]:
+                if target < arr[m] and target >= arr[l]:
+                    r = m - 1
+                else:
+                    l = m + 1
+            else: 
+                if target > arr[m] and target <= arr[r]:
+                    l = m + 1
+                else:
+                    r = m - 1
+        return - 1

ec56be46be8c2722c619b7288a933bfecdf9d5b3
Kaushal kumar <kaushal.kr.kr@gmail.com>
2025-11-29 23:26:58 +0530

    Add implementation for character replacement in a string

--- Two Pointer and Sliding Window/Longest Substring Without Repeating Characters/main.py
+++ Two Pointer and Sliding Window/Longest Substring Without Repeating Characters/main.py
@@ -1,21 +1,11 @@
-def is_window_valid(counts, k):
-    total = sum(counts)
-    max_count = max(counts)
-    return (total - max_count) <= k
-
-
-def characterReplacement(s, k):
-    i = j = 0
-    counts = [0] * 26
-    counts[ord(s[0]) - ord("A")] = 1
-    max_window = 0
-    while j < len(s):
-        if is_window_valid(counts, k):
-            max_window = max(max_window, j - i + 1)
-            j += 1
-            if j < len(s):
-                counts[ord(s[j]) - ord("A")] += 1
-        else:
-            counts[ord(s[i]) - ord("A")] -= 1
-            i += 1
-    return max_window
+def lengthOfLongestSubstring(s: str) -> int:
+    map = {}
+    maxWS = 0
+    i = 0
+    for j in range(len(s)):
+        if s[j] in map and map[s[j]] >= i:
+            i = map[s[j]] + 1
+        map[s[j]] = j
+        currWS = j - i + 1
+        maxWS = max(maxWS, currWS)
+    return maxWS

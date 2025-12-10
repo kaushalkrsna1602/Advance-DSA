e228122855dc99913d88cdae648752dd8f0ae73d
Kaushal kumar <kaushal.kr.kr@gmail.com>
2025-12-11 00:55:28 +0530

    Binary Tree Level Order Traversal [py] - recursion

--- Tree/Binary Tree Level Order Traversal/main.py
+++ Tree/Binary Tree Level Order Traversal/main.py
@@ -22,3 +22,29 @@ class Solution:
             result.append(level)
         
         return result
+    
+    
+    
+# recursion
+
+class Solution:
+    def levelOrder(self, root):
+        if not root:
+            return []
+        
+        ans = []
+
+        def dfs(node, level):
+            if len(ans) == level:
+                ans.append([])
+            ans[level].append(node.val)
+
+            if node.left:
+                dfs(node.left, level + 1)
+            if node.right:
+                dfs(node.right, level + 1)
+
+        dfs(root, 0)
+        return ans
+
+

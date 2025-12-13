69b7d4b3950cac52f6c03237922afdf0bd380189
Kaushal kumar <kaushal.kr.kr@gmail.com>
2025-12-13 10:45:43 +0530

    Path Sum [js] - iterative

--- Tree/Path Sum/index.js
+++ Tree/Path Sum/index.js
@@ -11,3 +11,29 @@ var hasPathSum = function(root, targetSum) {
         hasPathSum(root.right, targetSum - root.val)
     );
 };
+
+
+
+
+var hasPathSum2 = function(root, targetSum) {
+    if (root === null) return false;
+
+    const stack = [[root, targetSum]];
+
+    while (stack.length > 0) {
+        const [node, currSum] = stack.pop();
+
+        if (node.left === null && node.right === null) {
+            if (currSum === node.val) return true;
+        }
+
+        if (node.right !== null) {
+            stack.push([node.right, currSum - node.val]);
+        }
+        if (node.left !== null) {
+            stack.push([node.left, currSum - node.val]);
+        }
+    }
+
+    return false;
+};

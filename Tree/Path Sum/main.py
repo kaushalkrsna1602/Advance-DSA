0baac167048e4ababd0bfc557a28618a2f323395
Kaushal kumar <kaushal.kr.kr@gmail.com>
2025-12-13 10:41:10 +0530

    Path Sum [py] - bottom's up

--- Tree/Path Sum/main.py
+++ Tree/Path Sum/main.py
@@ -23,3 +23,20 @@ class Solution:
 
         traverse(root, 0)
         return ans
+
+
+
+# Bottom's Up
+
+class Solution:
+    def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
+        if not root:
+            return False
+
+        if not root.left and not root.right:
+            return targetSum == root.val
+
+        return (
+            self.hasPathSum(root.left, targetSum - root.val) or
+            self.hasPathSum(root.right, targetSum - root.val)
+        )

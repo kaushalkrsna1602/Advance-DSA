d6e0cdbc166bd54c5c3ae48714fb91acf6c5b035
Kaushal kumar <kaushal.kr.kr@gmail.com>
2025-12-15 10:19:04 +0530

    Symmetric Tree [py]

--- Tree/Symmetric Tree/main.py
+++ Tree/Symmetric Tree/main.py
@@ -20,3 +20,43 @@ class Solution:
             )
 
         return isMirror(root.left, root.right)
+
+
+# Iterative
+
+from collections import deque
+from typing import Optional
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+        if not root:
+            return True
+
+        q = deque([root.left, root.right])
+
+        while q:
+            p1 = q.popleft()
+            p2 = q.popleft()
+
+            if not p1 and not p2:
+                continue
+
+            if not p1 or not p2:
+                return False
+
+            if p1.val != p2.val:
+                return False
+
+            q.append(p1.left)
+            q.append(p2.right)
+            q.append(p1.right)
+            q.append(p2.left)
+
+        return True
